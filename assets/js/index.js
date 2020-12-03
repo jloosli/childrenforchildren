eaec6b7305945a517258f46a417a7cac43ddbaf4
Jared Loosli <jared-loosli@pluralsight.com>
2020-12-02 21:51:17 -0700

    Added overlay logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,38 @@ const options = {
 }
 
 const performerList = new List('main', options);
+
+
+/*******************************
+ Overlay
+ *****************************/
+
+const overlay = document.getElementById('overlay');
+
+function showVideo(videoID) {
+    overlay.style.display = 'grid';
+    const src = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
+    const iframe = createYoutubeIframe();
+    iframe.src = src;
+    overlay.querySelector('div').appendChild(iframe);
+
+}
+
+function createYoutubeIframe() {
+    const iframe = document.createElement('iframe');
+    iframe.title = "Youtube Video";
+    iframe.allowfullscreen = true;
+    return iframe;
+}
+
+function removeIframe() {
+    const parent = overlay.querySelector('div')
+    const iframe = parent.querySelector('iframe');
+    // parent.removeChild(iframe);
+    iframe.remove();
+}
+
+function closeVideo() {
+    overlay.style.display = 'none';
+    removeIframe();
+}
