9b9bec0411778c8abfba2dfb06028587622c383f
Jared Loosli <jared-loosli@pluralsight.com>
2020-12-08 13:57:01 -0700

    Updated Showvideo to include composer and piece if available

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,9 +9,8 @@ const options = {
 const performerList = new List('performers', options);
 
 let search = document.querySelector('.fuzzy-search');
-search.addEventListener('search', e=>{
-    if(!e.inputType){
-        console.log(e)
+search.addEventListener('search', e => {
+    if (!e.inputType) {
         performerList.search(search.value)
     }
 })
@@ -22,14 +21,17 @@ search.addEventListener('search', e=>{
 
 const overlay = document.getElementById('overlay');
 
-function showVideo(videoID) {
-    const src = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
-    const youtubeCode = createYoutubeCode(src);
+function showVideo(performer) {
+    const {youtubeID} = performer;
+    const src = `https://www.youtube.com/embed/${youtubeID}?autoplay=1`;
+    const youtubeCode = createYoutubeIframe(src);
+    const meta = createMetaDiv(performer)
     overlay.appendChild(youtubeCode);
+    overlay.appendChild(meta);
     overlay.style.display = 'grid';
 }
 
-function createYoutubeCode(src) {
+function createYoutubeIframe(src) {
     const div = document.createElement('div');
     div.setAttribute('class', 'youtube-wrapper');
 
@@ -44,11 +46,28 @@ function createYoutubeCode(src) {
     return div;
 }
 
-function removeYoutubeCode() {
+function createMetaDiv({fullName, talent, composer, piece}) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'meta')
+    div.setAttribute('style', 'text-align: center; color: white;')
+    const metaVals = []
+    fullName && metaVals.push(styleMeta('Peformer', fullName))
+    talent && metaVals.push(styleMeta(`Talent`, talent))
+    piece && metaVals.push(styleMeta(`Piece`, piece))
+    composer && metaVals.push(styleMeta(`Composer`, composer))
+    div.innerHTML = metaVals.join(``)
+    return div
+}
+
+function styleMeta(key, value) {
+    return `<div>${key}: <span class="h4">${value}</span></div>`
+}
+
+function removeOverlayContent() {
     overlay.innerHTML = '';
 }
 
 function closeVideo() {
     overlay.style.display = 'none';
-    removeYoutubeCode();
+    removeOverlayContent();
 }
